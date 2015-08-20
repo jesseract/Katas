1b91054290abb4526fa7a5ec41143fd44efe1cf7
Jesseract <jlpearce@gmail.com>
2015-08-20 10:18:14 -0400

    Coin changer kata day 8

---
+++ coin_changer_day_8.rb
@@ -0,0 +1,43 @@
+class Change
+  def coin_changer(amount)
+    change = []
+    coins = [25, 10, 5, 1]
+
+    coins.each do |coin|
+      number_of_coins = amount / coin
+      number_of_coins.times{ change << coin }
+      amount %= coin
+    end
+    change
+  end
+end
+
+
+
+
+require 'rspec/core'
+
+describe Change do
+  let(:change) { Change.new }
+
+  it "makes change for 100" do
+    expect(change.coin_changer(100)).to eq [25, 25, 25, 25]
+  end
+
+  it "makes change for 90 cents" do
+    expect(change.coin_changer(85)).to eq [25, 25, 25, 10]
+  end
+
+  it "makes change for 90 cents" do
+    expect(change.coin_changer(90)).to eq [25, 25, 25, 10, 5]
+  end
+
+  it "makes change for 93 cents" do
+    expect(change.coin_changer(93)).to eq [25, 25, 25, 10, 5, 1, 1, 1]
+  end
+
+  it "makes change for 0 cents" do
+    expect(change.coin_changer(0)).to eq []
+  end
+
+end
